f367aca58c1d46767184d5d33b9141c5a1049abb
Rachana Samboju <rachanasamboju@Rachanas-Air.lan>
2024-10-07 19:40:29 -0500

    firstcommmit

---
+++ addfile.py
@@ -0,0 +1,7 @@
+def addfunction (a,b):
+    c = a+b
+    return c
+
+def subfunction (a,b):
+    c = a-b
+    return c
---
+++ test.py
@@ -0,0 +1,4 @@
+from addfile import addfunction
+from addfile import subfunction
+print(addfunction(6, 3))
+print(subfunction(9,2))
